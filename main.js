a2796599f0ed2ba170d4e7b334b8b45d70494fee
Leonam Monteiro <lpdmonteiro@gmail.com>
2024-10-25 11:22:40 -0300

    ..

--- main.js
+++ main.js
@@ -2,16 +2,19 @@ import { arquivoSelecionado, encontrarArquivo } from './firmware.js';
 
 async function caminhoFirmware() {
 	try {
-		const pasta = '/home/lm/Documentos/teste-firmware/';
+		const pasta = 'C:/Users/itinerario/Desktop/GitHub/encontrarArquivo';
 		const extensao = '.css';
 
 		const arquivoSelecionado = await encontrarArquivo(pasta, extensao);
 		console.log('Arquivo selecionado:', arquivoSelecionado);
 		console.log('Caminho do firmware:', pasta + arquivoSelecionado);
+		const finalFirmwarePath = pasta + arquivoSelecionado;
+		return finalFirmwarePath;
 		// Faça algo com o arquivo selecionado...
 	} catch (error) {
 		console.error('Erro ao encontrar arquivo:', error);
 	}
 }
 
-caminhoFirmware();
+const finalFirmwarePath = await caminhoFirmware();
+console.log('Final Firmware Path:', finalFirmwarePath);
